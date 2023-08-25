480c898494bff0f20fe065496426d45fefff0343
Dash <dkshowarth@gmail.com>
2023-08-24 18:49:01 -0700

    move user routes into correct file

--- controller/routes/api/user.js
+++ controller/routes/api/user.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const { ObjectId } = require('mongoose');
+const User = require('../../../models/User')
+const Thought = require('../../../models/Thought')
+
 
 router.get('/', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.find({_id: {$eq: new ObjectId(req.params.id)}})
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
