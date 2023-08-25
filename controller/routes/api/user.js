7c310b20bb68f972a07e773c4ca2a85838cb7a23
Dash <dkshowarth@gmail.com>
2023-08-24 18:57:55 -0700

    add user put route

--- controller/routes/api/user.js
+++ controller/routes/api/user.js
@@ -22,12 +22,28 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const newUser = User.create(req.body)
+        const newUser = await User.create(req.body)
         res.status(200).json(newUser)
     } catch (err) {
         res.status(500).json(err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const userUpdate = await User.findOneAndUpdate(
+            {
+                _id: {$eq: req.params.id}
+            },
+            req.body,
+            {
+                new: true
+            }
+        )
+    } catch (err) {
+
     }
 })
 
