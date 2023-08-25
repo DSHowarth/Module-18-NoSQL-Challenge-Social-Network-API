cb4ae69d5763da3caa1774162d8674b7a7ddf27e
Dash <dkshowarth@gmail.com>
2023-08-24 17:14:37 -0700

    add user model

--- controller/routes/api/index.js
+++ controller/routes/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const thoughtRoute = require('./thought.js');
+const userRoute = require('./user.js');
+
+router.use('/users', userRoute);
+router.use('/thoughts', thoughtRoute);
 
 module.exports = router
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+})
