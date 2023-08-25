461a66042ad7ac19a771c2eaf48c62460c60c076
Dash <dkshowarth@gmail.com>
2023-08-24 18:45:40 -0700

    add all users get route

--- controller/routes/api/thought.js
+++ controller/routes/api/thought.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const User = require('../../../models/User')
+const Thought = require('../../../models/Thought')
 
-
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
--- controller/routes/api/user.js
+++ controller/routes/api/user.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+
+})
+
 module.exports = router
