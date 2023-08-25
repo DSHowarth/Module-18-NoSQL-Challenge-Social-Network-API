839df06877e28f8107208b9c496249a3db36f993
Dash <dkshowarth@gmail.com>
2023-08-24 18:48:35 -0700

    add single user get route

--- controller/routes/api/thought.js
+++ controller/routes/api/thought.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { ObjectId } = require('mongoose');
 const User = require('../../../models/User')
 const Thought = require('../../../models/Thought')
 
@@ -6,7 +7,16 @@ router.get('/', async (req, res) => {
     try{
         const users = await User.find();
         res.status(200).json(users);
-    } catch {
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.find({_id: {$eq: new ObjectId(req.params.id)}})
+        res.status(200).json(user)
+    } catch (err) {
         res.status(500).json(err)
     }
 })
