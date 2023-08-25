8d93c2b16f1075db57391aad090fad68ecd02b48
Dash <dkshowarth@gmail.com>
2023-08-24 18:09:38 -0700

    add friendcount virtual to user schema

--- models/User.js
+++ models/User.js
@@ -1,28 +1,37 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
-    },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
+    {
+        virtuals: {
+            friendCount: {
+                get() {
+                    return this.friends.length;
+                }
+            }
         }
-    ],
-    friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-})
+    })
