0c2cb9d0913982433b2e40242dce89baad5174ac
Dash <dkshowarth@gmail.com>
2023-08-24 19:22:40 -0700

    add thought get routes, create other route boilerplate

--- controller/routes/api/thought.js
+++ controller/routes/api/thought.js
@@ -4,7 +4,47 @@ const User = require('../../../models/User')
 const Thought = require('../../../models/Thought')
 
 router.get('/', async (req, res) => {
-    
+    try {
+        const thoughts = await Thought.find();
+        res.status(200).json(thoughts)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.find({ _id: {$eq: req.params.id}});
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.put('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
 
 module.exports = router
