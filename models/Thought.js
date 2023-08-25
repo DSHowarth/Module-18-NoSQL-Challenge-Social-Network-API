12ce405bf76144a2f15e5432c50afad7c9e88abd
Dash <dkshowarth@gmail.com>
2023-08-24 18:22:51 -0700

    add most of reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const dayjs = require('dayjs');
 
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: dayjs(),
+        //TODO: method
+    }
+})
+
 const thoughtSchema = new mongoose.Schema(
     {
         thoughtText: {
